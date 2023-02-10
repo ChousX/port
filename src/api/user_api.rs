2c32b88c2731f871e62bf23a8ec9119245b604d0
ChousX <gmanenergy@gmail.com>
2023-02-10 14:49:29 -0500

    .

--- src/api/user_api.rs
+++ src/api/user_api.rs
@@ -39,6 +39,12 @@ pub async fn get_user(db: Data<SurrealDBRepo>, path: Path<String>) -> HttpRespon
     }
 }
 
+pub async fn login(db: Data<SurrealDBRepo>) -> HttpResponse{
+    
+
+    todo!()
+}
+
 #[get("/users")]
 pub async fn get_users (db: Data<SurrealDBRepo>) -> HttpResponse {
     let result = UserBMC::get_all(db).await;
@@ -48,21 +54,3 @@ pub async fn get_users (db: Data<SurrealDBRepo>) -> HttpResponse {
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
    }
 }
-
-// pub async fn create_user(db: Data<SurrealDBRepo>, new_user: Json<User>) -> HttpResponse{
-//     if let User::Create { name, email, password } = new_user.0 {
-    
-//     }
-//     todo!()
-// }
-// #[get("/user/login/{cookie}")]
-// pub async fn login_user(db: Data<SurrealDBRepo>, login: Json<User>) -> HttpResponse{
-//     if let User::LogIn{name, password} = login.0 {
-        
-//     }
-//     todo!()
-// }
-
-// pub async fn delete_user() -> HttpResponse{
-
-// }
