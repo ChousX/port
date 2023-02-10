7095a1fe70facf82d44926892a06116784965524
ChousX <gmanenergy@gmail.com>
2023-02-10 14:11:38 -0500

    some things

--- src/model/user_model.rs
+++ src/model/user_model.rs
@@ -92,7 +92,7 @@ impl UserBMC{
     pub async fn get(db: Data<SurrealDBRepo>, tid: &str) -> Result<Object, Error> {
         let sql = "SELECT * FROM $th";
             
-            let tid = format!("todo:{}", tid);
+            let tid = format!("user:{}", tid);
 
             let vars: BTreeMap<String, Value> = map!["th".into() => thing(&tid)?.into()];
     
@@ -121,56 +121,3 @@ impl UserBMC{
         W(result.first()).try_into()
     }
 }
-
-// #[derive(Debug, Serialize, Deserialize)]
-// pub enum User{
-//     Create{
-//         name: String,
-//         email: String,
-//         password: String
-//     },
-//     LogIn{
-//         name: String,
-//         password: String
-//     }
-// }
-
-// impl From<User> for Value{
-//     fn from(val: User) -> Self {
-//         match val{
-//             User::Create { name, email, password } => {
-//                 map!{
-//                     "name".into() => name.into(),
-//                     "enail".into() => email.into(),
-//                     "password".into() => password.into(),
-//                 }.into()
-//             },
-//             User::LogIn { name, password } => {
-//                 map!{
-//                     "name".into() => name.into(),
-//                     "password".into() => password.into(),
-//                 }.into()
-//             }
-//         }
-//     }
-// }
-
-// impl Creatable for User {}
-
-// pub struct  UserBMC;
-
-// impl UserBMC {
-//     pub async fn create<T: Creatable>(db: Data<SurrealDBRepo>, tb: &str, data: T) -> Result<Object, Error>{
-//         let sql = "CREATE type::table($tb) CONTENT $data RETURN *";
-//         let data: Object = W(data.into()).try_into()?;
-//         let vars: BTreeMap<String, Value> = map!{
-//             "tb".into() => tb.into(),
-//             "data".into() => Value::from(data)
-//         };
-
-//         let response = db.ds.execute(sql, &db.ses, Some(vars), false).await?;
-//         let first_val =  response.into_iter().next().map(|r| r.result).expect("id not returned")?;
-
-//         W(first_val.first()).try_into()
-//     }
-// }
